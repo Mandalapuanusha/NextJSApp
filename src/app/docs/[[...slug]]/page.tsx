4df7f4bceb5ec6367dba37ef1f6a1dc60f8f0e5a
Mandalapuanusha <anushamandalapu01@gmail.com>
2024-05-25 00:00:43 -0400

    optional catch-all segments

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,10 +1,10 @@
 export default function Docs({ params }: {
     params: { slug: string[] }
 }) {
-    if (params.slug.length == 2) {
+    if (params.slug?.length == 2) {
         return (<h1>Viewing Docs for  feature {params.slug[0]} and {params.slug[1]} </h1>)
     }
-    else if (params.slug.length == 1) {
+    else if (params.slug?.length == 1) {
         return (<h1>Viewing Docs for  feature {params.slug[0]} </h1>)
     }
 
