3b6de803f2dd3e3adfa6bddc9a87d13f12e966e0
Mandalapuanusha <anushamandalapu01@gmail.com>
2024-05-27 12:16:32 -0400

    UI Link based component Navigation - Example 2 Client Side UI Navigation

--- src/app/products/ProductHomePage.tsx
+++ src/app/products/ProductHomePage.tsx
@@ -7,9 +7,12 @@ type Props = {
     }
 };
 export default function ProductsHome() {
+    const productId =100;
+
     return (<>
         <Link href="/"> Home</Link>
         <h1>Products List HomePage</h1> 
+        <h2> <Link href={`/products/${productId}`}> Product ${productId}</Link></h2>
         <h2> <Link href="/products/1"> Product 1</Link></h2>
         <h2> <Link href="/products/2"> Product 2</Link></h2>
         <h2> <Link href="/products/3"> Product 3</Link></h2>
