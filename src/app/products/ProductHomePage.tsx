42b7dba1546a36e33380c073289a738725bff518
Mandalapuanusha <anushamandalapu01@gmail.com>
2024-05-27 12:07:43 -0400

    UI Link based Component Navigation - Example 1 hardcoding dynamic routes

--- src/app/products/ProductHomePage.tsx
+++ src/app/products/ProductHomePage.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link"
+
+type Props = {
+    params:
+    {
+        productId: number;
+    }
+};
 export default function ProductsHome() {
     return (<>
-    <h1>Products List HomePage</h1> 
+        <Link href="/"> Home</Link>
+        <h1>Products List HomePage</h1> 
+        <h2> <Link href="/products/1"> Product 1</Link></h2>
+        <h2> <Link href="/products/2"> Product 2</Link></h2>
+        <h2> <Link href="/products/3"> Product 3</Link></h2>
     </>)
 }
