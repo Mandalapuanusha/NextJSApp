a111bcdce4a60d036c3a4d1655d25ff04e5d0b5d
Mandalapuanusha <anushamandalapu01@gmail.com>
2024-05-27 12:26:31 -0400

    Added replace prop for Link

--- src/app/products/ProductHomePage.tsx
+++ src/app/products/ProductHomePage.tsx
@@ -12,9 +12,10 @@ export default function ProductsHome() {
     return (<>
         <Link href="/"> Home</Link>
         <h1>Products List HomePage</h1> 
-        <h2> <Link href={`/products/${productId}`}> Product ${productId}</Link></h2>
+        <h2> <Link href={`/products/${productId}`}> Product {productId}</Link></h2>
         <h2> <Link href="/products/1"> Product 1</Link></h2>
-        <h2> <Link href="/products/2"> Product 2</Link></h2>
+        <h2> <Link href="/products/2" replace> Product 2</Link></h2>
+        {/* The replace prop will replace the current history State instead of adding a new URL to the stack. */}
         <h2> <Link href="/products/3"> Product 3</Link></h2>
     </>)
 }
