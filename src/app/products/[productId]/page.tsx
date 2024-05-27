4f464a742e44d521078d8c57eda23eeb38576cbe
Mandalapuanusha <anushamandalapu01@gmail.com>
2024-05-26 21:14:58 -0400

    Changes to add metadata dynamically

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,27 @@
-import { notFound } from "next/navigation"
 import ErrorProduct from "./ErrorProduct";
+import { Metadata } from "next";
 
-export default function ProductDetails({ params }: { params: { productId: number } }) {
+type Props = {
+    params:
+    {
+        productId: number;
+    }
+};
+
+export const generateMetadata = ({params}: Props): Metadata => {
+    return {
+        title: `Product ${params.productId}`,
+    }
+}
+
+// Using Jsx, we can pass it as below.
+// export const generateMetadata = ({ params }) => {
+//     return {
+//         title: `Product ${params.productId}`,
+//     }
+// }
+
+export default function ProductDetails({ params}:Props ) {
     if (params.productId <= 1000) {
         return (
             <h1>Product Details for product {params.productId}</h1>
@@ -9,7 +29,7 @@ export default function ProductDetails({ params }: { params: { productId: number
     }
     else if (params.productId > 1000) {
         return (
-            <ErrorProduct params={params}/>
+            <ErrorProduct params={params} />
         )
     }
 }
